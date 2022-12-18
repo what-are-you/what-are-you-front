78d42466d9c89fa625c52d6521eebb62e62bd2a8
김인우 <dlsdn166@gmail.com>
2022-12-18 17:58:28 +0900

    feat , 로그인, 회원가입 UI 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/login';
+import SignUp from './components/signup';
 
 function App() {
   return (
-
-  );
+    <div>
+      <SignIn />
+      {/* SignUp */}
+    </div>
+  )
 }
 
 export default App;
